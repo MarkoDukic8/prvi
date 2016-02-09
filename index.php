cc45d7e77d5d86005ca30e2481d8eca56fe71e90
tsd <MarkoDukic8>
2016-02-09 17:53:56 +0100

    Dodali smo kostur

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Naslov</title>
+	</head>
+
+	<body>
+		<h1>ECHO</h1>
+
+		<p>What the ECHO!?</p>
+		<p>Pufa samo ECHO-a</p>
+	</body>
+</html>
